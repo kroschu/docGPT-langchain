0ac62ac9c79819304b98f4c721bf8f9ef0c131cb
tukroschu@gmail.com <tukroschu@gmail.com>
2024-01-27 00:13:04 +0200

    update theme.py

--- components/theme.py
+++ components/theme.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 def theme() -> None:
-    st.set_page_config(page_title="Document GPT")
+    st.set_page_config(page_title="ІНДПОШИВ")
     st.image('./static/img/chatbot_v2.png', width=150)
