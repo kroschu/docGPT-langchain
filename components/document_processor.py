712945ade6cf20eba37498fd1d2f9cad20675fc5
tukroschu@gmail.com <tukroschu@gmail.com>
2024-01-27 00:04:58 +0200

    update document_processor.py

--- components/document_processor.py
+++ components/document_processor.py
@@ -9,11 +9,11 @@ from model import DocumentLoader
 def upload_and_process_document() -> list:
     st.write('#### Upload a Document file')
     browse, url_link = st.tabs(
-        ['Drag and drop file (Browse files)', 'Enter document URL link']
+        ['Завантажте файл бази знань', 'Вкажіть URL посилання до неї']
     )
     with browse:
         upload_file = st.file_uploader(
-            'Browse file (.pdf, .docx, .csv, `.txt`)',
+            'Відкрити (.pdf, .docx, .csv, `.txt`)',
             type=['pdf', 'docx', 'csv', 'txt'],
             label_visibility='hidden'
         )
@@ -23,7 +23,7 @@ def upload_and_process_document() -> list:
     with url_link:
         doc_url = st.text_input(
             "Enter document URL Link (.pdf, .docx, .csv, .txt)",
-            placeholder='https://www.xxx/uploads/file.pdf',
+            placeholder='https://2073.pp.ua/output.pdf',
             label_visibility='hidden'
         )
         if doc_url:
